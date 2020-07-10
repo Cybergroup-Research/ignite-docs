4bdba713f2b54d585260d29583b874d26116218a
Sebastian Labrador <slabrador09@gmail.com>
2020-07-09 19:56:45 -0500

    static build

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [
   {
     caption: 'User1',
     // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
+    // if it is not '/', like: '/ignite-docs/img/image.jpg'.
     image: '/img/undraw_open_source.svg',
     infoLink: 'https://www.facebook.com',
     pinned: true,
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'CG Ignite Docs', // Title for your website.
+  tagline: 'Low-Code Programming with CG Ignite',
+  url: 'https://slabrador09.github.io', // Your website URL
+  baseUrl: '/ignite-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  //   baseUrl: '/ignite-docs/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'ignite-docs',
+  organizationName: 'slabrador09',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -104,7 +104,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  //   repoUrl: 'https://github.com/facebook/ignite-docs',
 };
 
 module.exports = siteConfig;
